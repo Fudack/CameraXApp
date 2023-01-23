165af00e94c9bbecfd6575feda1d80aded2509e3
RodrigoMardonesAlvarez <rodrigomardones@protonmail.com>
2023-01-23 19:37:18 -0300

    Fixed dependencies versions

--- app/src/main/java/com/example/cameraxapp/ObjectDetection.kt
+++ app/src/main/java/com/example/cameraxapp/ObjectDetection.kt
@@ -1,4 +1,23 @@
 package com.example.cameraxapp
 
+import android.graphics.RectF
+
+
 class ObjectDetection {
+    data class ObjectPrediction(val location: RectF, val label: String, val score: Float)
+
+    private val locations = arrayOf(Array(OBJECT_COUNT) { FloatArray(4) })
+    private val labelIndices =  arrayOf(FloatArray(OBJECT_COUNT))
+    private val scores =  arrayOf(FloatArray(OBJECT_COUNT))
+
+    private val outputBuffer = mapOf(
+        0 to locations,
+        1 to labelIndices,
+        2 to scores,
+        3 to FloatArray(1)
+    )
+
+    companion object {
+        const val OBJECT_COUNT = 10
+    }
 }
